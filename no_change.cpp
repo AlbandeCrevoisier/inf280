758c53fac4ab74ea24ecc6df7e40b8c9c2500a14
ACH <albandecrevoisier@gmail.com>
2016-05-17 12:52:34 +0200

    No change: WIP.

---
+++ no_change.cpp
@@ -0,0 +1,21 @@
+/* No change */
+#include <iostream>
+
+int
+main(void)
+{
+    int c_nb;
+    cin >> c_nb;
+
+    for (int i = 0; i < c_nb; i++) {
+        int p, v_nb;
+        cin >> p >> v_nb;
+        int v[v_nb];
+        for (int j = 0; j < v_nb; j++)
+            cin >> v[j];
+        /* TODO */
+    }
+    return 0;
+}
+
+
