2987b1845964c4aa0fa73b723c6747b3160b4473
Otter Space <otter9space@gmail.com>
2016-05-18 14:15:48 +0200

    M&M: WIP

--- mice_and_maze.cpp
+++ mice_and_maze.cpp
@@ -13,14 +13,21 @@ main(void)
     scanf("%d", &c_nb);
     for (int c = 0; c < c_nb; c++) {
         int n, e, t, m;
+        map<int, int> p;
         vector<map<int, int>> g;
         scanf("%d %d %d %d", &n, &e, &t, &m);
         for (int i = 0; i < m; i++) {
             int a, b, l;
             scanf("%d %d %d", &a, &b, &l);
-            g[a].insert(pair<int, int>(b, l));
+            g[b].insert(pair<int, int>(a, -l));
         }
+
+        /* reverse Dijkstra-ih */
+        p.insert(pair<int, pair<int, int>>(e, pair<int, int>(e, t));
+
     }
+    
+
 
     return 0;
 }
