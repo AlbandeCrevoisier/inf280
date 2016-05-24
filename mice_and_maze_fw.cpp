71c8f33c40dc991473d6a53dde3f59e307c23302
Otter Space <otter9space@gmail.com>
2016-05-24 13:49:53 +0200

    M&M: WIP

--- mice_and_maze_fw.cpp
+++ mice_and_maze_fw.cpp
@@ -8,25 +8,25 @@ main(void)
 {
 	int c_nb;
 	scanf("%d", &c_nb);
-	for (int i = 0; i < c_nb; i++) {
+	for (int c = 0; c < c_nb; c++) {
 		int n, e, t, m, s = 0;
 		int g[100][100];
-		for (int j = 0; j < 100; j++)
-			for (int k = 0; k < 100; k++)
-				g[j][k] = INT_MAX;
+		for (int i = 0; i < 100; i++)
+			for (int j = 0; j < 100; j++)
+				g[i][j] = INT_MAX;
 		scanf("%d %d %d %d", &n, &e, &t, &m);
-		for (int j = 0; j < m; j++) {
+		for (int i = 0; i < m; i++) {
 			int a, b, c;
 			scanf("%d %d %d", &a, &b, &c);
 			g[a][b] = c;
 		}
 		/* Floyd - Warshall */
-		for (int j = 0; j < n; j++)
-			for (int k = 0; k < n; k++)
-				for (int l = 0; l < n; l++)
-					g[k, l] = min(g[k, j] + g[j, l]);
-		for (int j = 0; j < n; j++)
-			if (g[e, j] >= 0)
+		for (int k = 0; k < n; k++)
+			for (int i = 0; i < n; i++)
+				for (int j = 0; j < n; j++)
+					g[i][j] = min(g[i][j], g[i][k] + g[k][j]);
+		for (int i = 0; i < n; i++)
+			if (g[i][e] <= t)
 				s++;
 		printf("%d\n\n", s);
 	}
