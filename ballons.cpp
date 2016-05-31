35716e5e5eef8c858cf0c622ac723c54b3b8ea8f
Otter Space <otter9space@gmail.com>
2016-05-31 16:01:18 +0200

    Ballons: WIP start.

---
+++ ballons.cpp
@@ -0,0 +1,14 @@
+/* Ballons in a box */
+#include <stdio.h>
+#include <math.h>
+#define PI 3.1415926535
+#define MAX 720 /* 6! *!
+
+int
+main(void)
+{
+	int nb_c;
+	scanf("%d", &nb_c);
+	for (int c = 0; c < nb_c; c++) {
+		int a[3]; b[3];
+		int s[MAX];
