9aa225c4c2f15d4192b109b4f3cb33ef87f4bd1a
Otter Space <otter9space@gmail.com>
2016-05-31 16:52:51 +0200

    Ballons: WIP init and permute.

--- ballons.cpp
+++ ballons.cpp
@@ -4,7 +4,8 @@
 #include <algorithm>
 #include <math.h>
 #define PI 3.1415926535
-#define MAX 720 /* 6! */
+#define N 6
+#define MAX 720 /* N! */
 using namespace std;
 
 int
@@ -14,12 +15,18 @@ main(void)
 	scanf("%d", &nb_p);
 	while (nb_p != 0) {
 		int a[3]; b[3];
-		int p[6][4]; /* 1-3: position, 4: radius */
+		int p[N][4]; /* 1-3: position, 4: radius */
 		int s[MAX];
+		int pr[N];
+		for (int 
 		scanf("%d %d %d", &a[0], &a[1], &a[2]);
 		scanf("%d %d %d", &b[0], &b[1], &b[2]);
 		for (int i = 0; i < nb_p; i++)
 			scanf("%d %d %d", &p[i][0], &p[i][1], &p[i][2]);
+
+		while (next_permutation(pr, pr + N) {
+			/* compute volume and put it in s */
+		}
 	}
 	return 0;
 }
