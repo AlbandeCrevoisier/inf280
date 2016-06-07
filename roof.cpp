65dc4713b60c10ba4a11df64543ac25d8eaa0220
Otter Space <otter9space@gmail.com>
2016-06-07 14:35:43 +0200

    Roof: WIP

--- roof.cpp
+++ roof.cpp
@@ -12,10 +12,24 @@ main(void)
 		int n;
 		scanf("%d", &n);
 		vector<vector<int>> r;
+		vector<vector<int>> w_dep;
 		for (int i = 0; i < n; i++) {
 			int x1, y1, x2, y2;
 			scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
 			r.push_back(vector<int>() = {x1, y1, x2, y2});
+		}
+		/* Compute where water falls. */
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i == j)
+					continue;
+				if (r[i][0] <= r[j][2] && r[j][2] <= r[i][2] &&
+				r[j][2] < r[j][1]) {
+					int turn = (r[i][2] - r[i][0]) * (r[j][3] - r[i][3]) - (r[j][2] -r[i][2]) * (r[i][3] - r[i][1]);
+					
+					w_dep[i].push_back(j);
+				}
+			}
 		}
 	}
 	return 0;
