47ce337a2b145c98c7a2dcd27440a9251f9bf3df
Otter Space <otter9space@gmail.com>
2016-06-13 12:51:39 +0200

    Add heat.

---
+++ heat.cpp
@@ -0,0 +1,17 @@
+/* Fighting the Heat */
+#include <stdio.h>
+#define N 40
+using namespace std;
+
+int
+main(void)
+{
+	bool h[N][N] = {false};
+	int n, m;
+	scanf("%d %d", &n, &m);
+	for( int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			String s;
+			scanf("%s", s);
+	return 0;
+}
