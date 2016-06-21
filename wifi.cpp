4828982613b0be88ab6d28c7bf22d301a985f8cb
Otter Space <otter9space@gmail.com>
2016-06-21 13:23:17 +0200

    Add wifi.

---
+++ wifi.cpp
@@ -0,0 +1,40 @@
+/* Optimal wifi coverage */
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int
+main(void)
+{
+	int nb_c;
+	scanf("%d", &nb_c);
+	for (int c = 0; c < nb_c; c++) {
+		int n, m, d;
+		vector<int, int> w;
+		scanf("%d %d", &n, &m);
+		for (int i = 0; i < m; i++) {
+			int tmp;
+			scanf("%d", &tmp);
+			w.push_back(pair<int, int>(tmp, tmp));
+		}
+
+		for (int i = 0; i < (m - n); i++) {
+			int idx_min;
+			int min = w.back().second;
+			for (int j = 0; j < (w.size() - 1); j++) {
+				if (w[j + 1].second - w[j].first) < min) {
+					min = w[j + 1].second - w[j].first;
+					idx_min = j;
+				}
+			}
+			w[idx_min].second = w[idx_min + 1].second;
+			w.erase(w.begin() + idx_min + 1);
+		}
+		d = w.back().second;
+		for (auto r : w)
+			if ((r.second - r.first) < d)
+				d = r.second - r.first;
+		printf("%f.1", (double) d);
+	}
+	return 0;
+}
